1b6132f3dafcd41097ba17451bdf7b77ba9b42b3
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <116402204+GREEDYS0NNY@users.noreply.github.com>
2023-11-04 18:48:16 +0100

    Add files via upload

---
+++ Laboratorium1/Zadanie3/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace Zadanie3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MenuControl();
+        }
+        static void PrintMenu()
+        {
+            Console.WriteLine("Wybierz operacje:");
+            Console.WriteLine("1. Wyświetlanie tablicy od pierwszego do ostatniego indeksu;");
+            Console.WriteLine("2. Wyświetlanie tablicy od ostatniego do pierwszego indeksu;");
+            Console.WriteLine("3. Wyświetlanie elementów o nieparzystych indeksach;");
+            Console.WriteLine("4. Wyświetlanie elementów o parzystych indeksach;"); ;
+            Console.WriteLine("5. Wyjscie.");
+            Console.Write("Twoj wybor: ");
+        }
+        static void MenuControl()
+        {
+            do
+            {
+                Console.ReadKey();
+                Console.Clear();
+
+                PrintMenu();
+                int SelectedOperation = Convert.ToInt32(Console.ReadLine());
+
+                if (SelectedOperation == 1)
+                    PrintArray(GetArray());
+
+                else if (SelectedOperation == 2)
+                    PrintReverseArray(GetArray());
+
+                else if (SelectedOperation == 3)
+                    PrintOddArray(GetArray());
+
+                else if (SelectedOperation == 4)
+                    PrintEvenArray(GetArray());
+
+                else if (SelectedOperation == 5)
+                    Environment.Exit(0);
+
+                else
+                    Console.WriteLine("Nieprawidlowa opcja!");
+            }
+            while (true);
+        }
+        static double[] GetArray()
+        {
+            double[] array = new double[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Wpisz liczbe pod indeksem {i}: ");
+                array[i] = InputDouble();
+            }
+            return array;
+
+        }
+        static double InputDouble()
+        {
+            double Value = Convert.ToInt32(Console.ReadLine());
+            return Value;
+        }
+        static void PrintArray(double[] array)
+        {
+            Console.WriteLine();
+            foreach (double item in array)
+            {
+                Console.Write(item + " ");
+            }
+        }
+        static void PrintReverseArray(double[] array)
+        {
+            Console.WriteLine();
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+        static void PrintOddArray(double[] array)
+        {
+            Console.WriteLine();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if ((i % 2) != 0)
+                    Console.Write(array[i] + " ");
+            }
+        }
+        static void PrintEvenArray(double[] array)
+        {
+            Console.WriteLine();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if ((i % 2) == 0)
+                    Console.Write(array[i] + " ");
+            }
+        }
+    }
+}
