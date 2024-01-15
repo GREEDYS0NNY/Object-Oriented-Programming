7fef1a30338f9a45b14c24017300f8299de40b14
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <116402204+GREEDYS0NNY@users.noreply.github.com>
2024-01-16 00:36:40 +0100

    Delete Laboratorium7/Zadanie1_2 directory

--- Laboratorium7/Zadanie1_2/Address.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Zadanie1
-{
-    internal class Address
-    {
-        public string Country { get; set; }
-        public string City { get; set; }
-        public string Street { get; set; }
-    }
-}
