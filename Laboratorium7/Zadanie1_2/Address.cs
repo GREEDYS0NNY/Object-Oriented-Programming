f9e1a2be188bd298d693b2ca35620285978def2f
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <116402204+GREEDYS0NNY@users.noreply.github.com>
2024-01-16 00:41:19 +0100

    Add files via upload

---
+++ Laboratorium7/Zadanie1_2/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace Zadanie1
+{
+    internal class Address
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+    }
+}
